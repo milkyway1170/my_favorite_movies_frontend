0a8a22505c53a51c19a02635ddf2383eff2353ae
Владислав Бублик <bublik1170@gmail.com>
2022-05-02 18:18:38 +0300

    Edited export list  in getFunctions

--- src/utils/getFunctions.ts
+++ src/utils/getFunctions.ts
@@ -1,6 +1,6 @@
 import { IDeleteOrInsertInArray, IGenreItem } from "@types";
 
-const getGenresList = (
+export const getGenresList = (
   setGenresList: React.Dispatch<React.SetStateAction<IGenreItem[]>>
 ) => {
   fetch(
@@ -8,41 +8,36 @@ const getGenresList = (
       (process.env.REACT_APP_API_KEY ?? "") +
       "&language=en-US"
   )
-    .then((response) => { 
+    .then((response) => {
       return response.json();
     })
     .catch((error) => {
       console.log(error);
     })
     .then((data) => {
-      setGenresList(
-        data.genres.map((genreItem:IGenreItem) => (genreItem))
-      );
+      setGenresList(data.genres.map((genreItem: IGenreItem) => genreItem));
     });
 };
 
-const getPoster = (posterPath: string |undefined) => {
+export const getPoster = (posterPath: string | undefined) => {
   return (process.env.REACT_APP_GET_POSTER ?? "") + posterPath;
 };
 
-const getData = (param: string) => {
+export const getData = (param: string) => {
   const data = JSON.parse(localStorage.getItem(param) || "{}");
   return data;
 };
 
-const deleteOrInsertInArray  = ({checkedArray, checkedArrayItem}:IDeleteOrInsertInArray): number[] =>{
+export const deleteOrInsertInArray = ({
+  checkedArray,
+  checkedArrayItem,
+}: IDeleteOrInsertInArray): number[] => {
   let resultList: number[] = [];
-  resultList = checkedArray.indexOf(checkedArrayItem) >= 0
-    ? (checkedArray.filter(
-        (arrayItem: number ) => arrayItem !== checkedArrayItem
-      ))
-    : ([...checkedArray, checkedArrayItem]);
+  resultList =
+    checkedArray.indexOf(checkedArrayItem) >= 0
+      ? checkedArray.filter(
+          (arrayItem: number) => arrayItem !== checkedArrayItem
+        )
+      : [...checkedArray, checkedArrayItem];
   return resultList;
-}
-
-export {
-  deleteOrInsertInArray,
-  getGenresList,
-  getData,
-  getPoster,
 };
